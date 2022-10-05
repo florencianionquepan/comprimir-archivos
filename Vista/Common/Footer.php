4ce6f59eb6570d9f5b69b9689bd9b9a1dad55224
darianasm <darianagsm@gmail.com>
2022-10-05 20:39:21 -0300

    Vista

---
+++ Vista/Common/Footer.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="Assets/css.css">
+<footer class="page-footer font-small" style="background-color: rgb(175, 124, 223);>
+
+    <!-- Copyright -->
+    <div class="container">
+        <div class="footer-copyright text-center py-3 text-light" >Flor y Dari:)
+        </div>
+    </div>
+    
+    </body>
+
+    </html>
--- Vista/index.php
+++ Vista/index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Compresión de archivos</title>
-    <link href="bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+<?php include_once('../Vista/Common/Header.php')?>
     <div class="container-md">
         <div class="row m-5 text-center justify-content-center align-items-center">
             <form method="post" action="GenerarZip.php" enctype="multipart/form-data" class="col-12 col-md-8 col-lg-6 col-xl-4">
@@ -16,5 +7,4 @@
             </form>
         </div>
     </div>
-</body>
-</html>
+<?php include_once('../Vista/Common/Footer.php')?>

d8a2cd3171f7de278424d2a0511315515896f9ed
FlorÑonquepan <florencianionquepan@outlook.com>
2022-10-08 11:56:12 -0300

    Agrega opción de descarga y alerta en caso de que no se haya generado el zip

--- Vista/accionZip.php
+++ Vista/accionZip.php
@@ -1,9 +1,26 @@
 <?php
+include_once('../Vista/Common/Header.php');
 include_once("./../Control/C_archivoComprimido.php");
 
 $objetoZip=new C_archivoComprimido;
 $file=$objetoZip->moverArchivo($_FILES);
-$objetoZip->generarComprimido($file);
-
+$resultados=$objetoZip->generarComprimido($file);
 
 ?>
+ <div class="container-md">
+    <div class="row m-5 text-center justify-content-center align-items-center">
+        <?php if($resultados[0]){?>
+            <div class="col-12 col-md-8 col-lg-6 col-xl-4 alert alert-secondary" role="alert">
+            Ahora puedes descargar  <a class="alert-link" href='<?php echo $resultados[1]?>'>tu fichero ZIP ♥</a>
+            </div>
+        <?php
+        }else{ ?>
+            <div class="col-12 col-md-8 col-lg-6 col-xl-4 alert alert-danger" role="alert">
+            No fue posible generar el ZIP <br> <a class="alert-link" href='./index.php'> VOLVER                     </a>
+            </div>
+            <?php
+        } 
+        ?>
+    </div>
+</div>
+<?php include_once('../Vista/Common/Footer.php')?>

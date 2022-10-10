5da1ce0208649249b6b0edd386356efd7b5446cb
FlorÑonquepan <florencianionquepan@outlook.com>
2022-10-10 15:34:02 -0300

    se guardan archivos al seleccionarlos

--- Vista/index.php
+++ Vista/index.php
@@ -2,9 +2,8 @@
     <div class="container-md">
         <div class="row m-5 text-center justify-content-center align-items-center">
             <form method="post" action="accionZip.php" enctype="multipart/form-data" class="col-12 col-md-8 col-lg-6 col-xl-4">
-                    <input type="file" name="archivos[]" class="form-control" id="archivos[]" multiple required>
-                    <button class="btn btn-outline-secondary my-3" type="button" id="envio">SUBIR</button>
-                    <button class="btn btn-outline-secondary my-3" type="submit">COMPRIMIR</button>
+                    <input type="file" name="archivos[]" class="form-control" id="archivos[]" multiple>
+                    <button class="btn btn-outline-secondary my-3" type="button">COMPRIMIR</button>
             </form>
         </div>
     </div>

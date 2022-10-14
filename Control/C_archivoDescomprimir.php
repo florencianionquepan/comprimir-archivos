f8d3b278963610a3df2a91250080a42ef1c59944
FlorÑonquepan <florencianionquepan@outlook.com>
2022-10-14 13:28:50 -0300

    Identa archivo js

--- Control/C_archivoDescomprimir.php
+++ Control/C_archivoDescomprimir.php
@@ -12,77 +12,21 @@ class C_archivoDescomprimido{
         $archivo=$zipUser["zip"]["name"];
         $zip = new ZipArchive();
         $resultado=false;
-        if($zip->open($locacion)===TRUE) {
+        //quiero que el archivo a descomprimir sea de solo lectura
+        if($zip->open($locacion, ZipArchive::RDONLY)===TRUE) {
             $zip->extractTo('./../Descomprimidos');
             $zip->close();
             $resultado=true;
         }else{
-            echo "not";
+            echo "no era archivo zip";
         }
         return $resultado;
-    }
-
-    function descargarArchivos(){
-        $estructura='./../Descomprimidos';
-
-        header("Cache-Control: public");
-        header("Content-Description: File Transfer");
-        header("Content-Disposition: attachment; filename=$fileName");
-        header("Content-Type: application/zip");
-        header("Content-Transfer-Encoding: binary");
     }
 
     function eliminar(){
         $estructura='./../Descomprimidos';
         $eliminada=rmdir($estructura, 0700);
         return $eliminada;
-    }
-
-    function obtenerArchivos(){
-        $listaArchivos=[];
-        $i=0;
-        //Chequear que exista la carpeta Descomprimidos:
-        $ruta='./../Descomprimidos';
-        if (is_dir($ruta)){
-            //Abre un gestor de directorios para la ruta indicada
-            $gestor = opendir($ruta);
-            //Recorre todos los archivos del directorio
-            while (($archivo = readdir($gestor)) !== false){
-                $ruta_completa = $ruta . "/" . $archivo;
-                //Se muestran todos los archivos y carpetas excepto "." y ".."
-                if ($archivo != "." && $archivo != ".."){
-                    //Si es un directorio se recorre recursivamente
-                    if (is_dir($ruta_completa)){
-                        echo "<li>" . $archivo . "</li>";
-                        $carpeta[$i]=obtenerArchivos($ruta_completa);
-                        $i++;
-                        array_push($listaArchivos,$carpeta[$i]);
-                    }else{
-                        echo "<li>" . $archivo . "</li>";
-                        array_push($listaArchivos,$archivo);
-                    }
-                }
-            }
-            //Cierra el gestor de directorios
-            closedir($gestor);
-            echo "</ul>";
-        }else{
-            echo "No es una ruta de directorio valida<br/>";
-        }
-        return $listaArchivos;
-    }
-    
-    function descargar($fichero){
-        $basefichero = basename($fichero);
-        if(!empty($basefichero)){
-            header("Cache-Control: public");
-            header("Content-Description: File Transfer");
-            header( 'Content-Type: application/octet-stream');
-            header( 'Content-Length:'.filesize($fichero));
-            header( 'Content-Disposition:attachment;filename='.$basefichero);
-            readfile($fichero);
-            exit;
-        }    
     }
 }
 

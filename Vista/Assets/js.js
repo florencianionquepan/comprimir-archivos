8f5575fb5f683ff7a165cd6135001662018fe24e
Dariana <darianagsm@gmail.com>
2022-10-14 10:46:36 -0300

    mostrar nombres de los archivos

---
+++ Vista/Assets/js.js
@@ -0,0 +1,36 @@
+const inputFile=document.querySelector('input');
+    inputFile.onchange=function(){
+
+    subir();
+    if(inputFile.files.length>0){
+    let formData=new FormData();
+    for(let i=0;i<inputFile.files.length;i++){
+        formData.append("archivos[]",inputFile.files[i]);
+    }
+    fetch("guardar.php",{
+        method: 'POST',
+        body:formData,
+    })
+    .then(resp=>successMessage(resp.json()))
+    }}
+
+
+    function successMessage(resp){
+    console.log(resp);
+    Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Tus archivos fueron subidos',
+    showConfirmButton: false,
+    })
+    }
+
+    function subir(){
+    archivos = inputFile.files;
+    for(i = 0; i < archivos.length; i++){
+    var table = document.getElementById("nombresArchivos");
+    var cantFilas = table.getElementsByTagName("tr").length;
+    var row = table.insertRow(cantFilas);
+    row.innerHTML += '<td>'+archivos[i].name+'</td>'; 
+    }
+    }

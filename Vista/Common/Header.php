e09c73de5b82aca69ce45b3a4221a1acef582cb5
Dariana <darianagsm@gmail.com>
2022-10-15 13:09:02 -0300

    estructura

--- Vista/Common/Header.php
+++ Vista/Common/Header.php
@@ -15,7 +15,8 @@
     <script type='text/javascript' src='../node_modules/bootstrap/dist/js/bootstrap.min.js'></script>
    
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -23,15 +24,35 @@
     <div>
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
+            
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active text-light fs-5" aria-current="page" href="../Vista/Index.php">Comprimir Archivos</a>
+                            <a class="nav-link active text-light fs-5" aria-current="page" href="../Vista/Index.php">Home  | </a>
                         </li>
 
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-light fs-5" href="../Vista/Index.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Comprimir archivos
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="comprimir.php">ZipArchive</a></li>
+                                <li><a class="dropdown-item" href="#">PclZip</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-light fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Descomprimir archivos
+                            </a>
+                            <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">ZipArchive</a></li>
+                                <li><a class="dropdown-item" href="#">PclZip</a></li>
+                            </ul>
+                        </li>
                     </ul>
                 </div>
             </div>
--- Vista/index.php
+++ Vista/index.php
@@ -1,31 +1,13 @@
 <?php 
     include_once('../Vista/Common/Header.php');
-    
-    if(file_exists('../Vista/Comprimidos.zip')){
-        unlink('../Vista/Comprimidos.zip');
-    }
 ?>
-    
-    <div class="container-md align-items: center">
-        <div class="row pt-5 pb-5 ">
-            <div class="col row pt-5 pb-5 mx-5 justify-content-center">
-                <form method="post" action="accionZip.php" enctype="multipart/form-data" class="col-8 row align-items-center" id = "form" novalidate>
-                    <input type="file" name="archivos[]" id='archivos' class="form-control" id="archivos[]" multiple></input>
-                   <button class="btn btn-outline-secondary my-3" type="submit" id="comprimir">COMPRIMIR ARCHIVO/S</button>
-                </form>
-            </div>
-    
-            <div class="col row pt-5 pb-5 mx-5 justify-content-center">
-                ARCHIVOS
-                <table class="table table-hover" id='nombresArchivos'>
-                    <tr></tr>
-                </table>
-            </div>
-        </div>
-    </div>
 
-    <script type='text/javascript' src='./Assets/js.js'></script>
-   
+<link rel="stylesheet" href="Assets/estilo.css">
+<div id="indexElement">
+  <h1>PWD</h1>
+  <img id="imgIndex" src="Assets/original.jpg" alt="gato"/>
+</div>
+
 <?php
 include_once('../Vista/Common/Footer.php');
 ?>

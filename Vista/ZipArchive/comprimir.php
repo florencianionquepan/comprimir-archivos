6e387f79aef84e1f30e47161f0b43e0c744ed51a
FlorÑonquepan <florencianionquepan@outlook.com>
2022-10-17 12:42:57 -0300

    Se deja en comprimir el llamado a los métodos

--- Vista/ZipArchive/comprimir.php
+++ Vista/ZipArchive/comprimir.php
@@ -1,31 +1,18 @@
 <?php 
     include_once('../../Vista/Common/Header.php');
     include_once("../../Control/ZipArchive/C_archivoComprimido.php");
-/*     
-    if(file_exists('../../Vista/ZipArchive/Comprimidos.zip')){
-        unlink('../../Vista/ZipArchive/Comprimidos.zip');
-    }
 
-    $carpeta = @scandir('../../Uploads');
-    $files=[];
-        if (count($carpeta) > 2){
-            if($handler=opendir('../../Uploads')){
-                while(false !== ($file=readdir($handler))){
-                    array_push($files,$file);
-                }
-                closedir($handler);
-            }
+    $estructura='./../../Uploads';
+    $objetoZip=new C_archivoComprimido;
+    $objetoZip->generarCarpeta($estructura);
 
-            $zip=new C_archivoComprimido;
-            $zip->limpiarDirectorio($files);
-        }     */
 ?>
     
     <div class="container-md align-items-center mb-5">
         <div class="row py-5 justify-content-center">
             <div class="col-10 col-md-8 col-lg-5 py-5 my-3 cuadro justify-content-center text-center">
                 <form method="post" action="accionZip.php" enctype="multipart/form-data" class="align-items-center" id = "form">
-                    <input type="file" name="archivos[]" id='archivos' class="form-control" id="archivos[]" multiple></input>
+                    <input type="file" name="archivos[]" class="form-control" id="archivos[]" multiple></input>
                    <button class="btn btn-outline-secondary my-3" disabled="disabled" type="submit" id="comprimir">COMPRIMIR ARCHIVO/S</button>
                 </form>
             </div>
@@ -40,8 +27,7 @@
         </div>
     </div>
 
-    <script type='text/javascript' src='../Assets/js/java.js'></script>
-    <script type='text/javascript' src='../Assets/js/js.js'></script>
+    <script type='text/javascript' src='../Assets/js/main.js'></script>
    
 <?php
 include_once('../../Vista/Common/Footer.php');

4b4d9e40d7d70e13d2fd95c1748f37f1cb18342b
FlorÑonquepan <florencianionquepan@outlook.com>
2022-10-18 12:45:38 -0300

    Elimina archivo

--- Vista/PclZip/guardar.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("./../../Control/ZipArchive/C_archivoComprimidoPcl.php");
-
-$objetoZip=new C_archivoComprimidoPcl;
-$folder='./../../Uploads/';
-$objetoZip->moverArchivo($_FILES,$folder);
-
-
-?>

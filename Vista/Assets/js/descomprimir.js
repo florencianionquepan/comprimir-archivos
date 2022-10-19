d814d4af45cb3805f3747dae87106e24413f71eb
Dariana <darianagsm@gmail.com>
2022-10-18 23:41:22 -0300

    verifica que sea un archivo zip

--- Vista/Assets/js/descomprimir.js
+++ Vista/Assets/js/descomprimir.js
@@ -1,1 +1,19 @@
 const inputFile=document.querySelector('input');
+
+inputFile.onchange=function(){
+    var archivoZip = inputFile.value;
+
+    if(!archivoZip.endsWith('.zip')){
+        inputFile.value = '';
+        errorMessage();
+    }
+}
+
+function errorMessage(){
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Solo se admiten archivos tipo Zip',
+        confirmButtonColor: 'rgb(156, 51, 253)',
+      })
+}

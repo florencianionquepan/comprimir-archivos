9cb99e838d8495c3ebc9169dafa85dcf8f7bd2a0
FlorÑonquepan <florencianionquepan@outlook.com>
2022-10-20 12:54:57 -0300

    deja todos los scripts en el header

--- Vista/Common/Header.php
+++ Vista/Common/Header.php
@@ -17,6 +17,9 @@ include_once('./../../configuracion.php');
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="../../node_modules/sweetalert2/dist/sweetalert2.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script type='text/javascript' src='../../node_modules/bootstrap/dist/js/bootstrap.min.js'></script>
+    <script type='text/javascript' src='../../node_modules/sweetalert2/dist/sweetalert2.min.js'></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
 
 <body>

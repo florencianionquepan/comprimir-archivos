162ac0d2bfd1588991c5471be93a295627d776df
FlorÑonquepan <florencianionquepan@outlook.com>
2022-10-20 11:21:08 -0300

    ajustes de estilo

--- Vista/Home/index.php
+++ Vista/Home/index.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="../Assets/css/estilo.css">
 <link href="http://fonts.cdnfonts.com/css/cascadia-code" rel="stylesheet">
 
-<div class="container-md mt-3 text-center">
+<div class="container-md mt-3">
 
 <div class="row align-items-center justify-content-center">
     <div class="col-10 text-center mb-3">
@@ -32,19 +32,19 @@
 
   <div class="row align-items-center border-top justify-content-center">
     <div class="col-10 text-center pt-3">
-      <h3>Metodos utilizados para generar zip</h3>
+      <h3>Métodos utilizados para generar zip</h3>
     </div>
   </div>
   <div class="row align-items-center justify-content-center">
     <div class="col-10 col-md-6 col-lg-5">
     <a href='https://www.php.net/manual/es/ziparchive.open' class='cascadia-code'>ZipArchive::open($nombreZip)</a><br>
-    <a href='https://www.php.net/manual/es/ziparchive.addfile' class='cascadia-code'>ZipArchive::addFile($rutaFichero)</a><br>
+    <a href='https://www.php.net/manual/es/ziparchive.addfile' class='cascadia-code'>ZipArchive::addFile($rutaArchivo)</a><br>
     <a href='https://www.php.net/manual/es/ziparchive.close' class='cascadia-code'>ZipArchive::close()</a><br>
-    <img class="img-fluid" src="../Imagenes/generarComprimido.png">
+    <img class="img-fluid mt-2" src="../Imagenes/generarComprimido.png">
     </div>
     <div class="col-10 col-md-6 col-lg-5">
-    <a href='https://nullpoint3r.wordpress.com/2011/06/05/pclzip-trabajando-con-ficheros-comprimidos-en-php/' class='cascadia-code'>PclZip::add($rutaFichero)</a><br>
-    <img class="img-fluid" src="../Imagenes/comprimidoPclzip.png">
+    <a href='https://nullpoint3r.wordpress.com/2011/06/05/pclzip-trabajando-con-ficheros-comprimidos-en-php/' class='cascadia-code'>PclZip::add($rutaArchivo)</a><br>
+    <img class="img-fluid mt-2" src="../Imagenes/comprimidoPclzip.png">
     </div>
   </div>
   
@@ -53,21 +53,21 @@
 
   <div class="row align-items-center  border-top justify-content-center mt-3 ">
     <div class="col-10 text-center pt-3 ">
-      <h3>Metodos utilizados descomprimir zip</h3>
+      <h3>Métodos utilizados para descomprimir zip</h3>
     </div>
     
     <div class="row align-items-center justify-content-center pb-5">
     
     <div class="col-10 col-md-6 col-lg-5 ">
     <a href='https://www.php.net/manual/es/ziparchive.open' class='cascadia-code'>ZipArchive::open($rutaArchivoZip)</a><br>
-    <a href='https://www.php.net/manual/en/ziparchive.extractto.php'>ZipArchive::extractTo($ruta)</a><br>
+    <a href='https://www.php.net/manual/en/ziparchive.extractto.php' class='cascadia-code'>ZipArchive::extractTo($ruta)</a><br>
     <a href='https://www.php.net/manual/es/ziparchive.close' class='cascadia-code'>ZipArchive::close()</a><br>
-    <img class="img-fluid" src="../Imagenes/descomprimirzip.png">
+    <img class="img-fluid mt-2" src="../Imagenes/descomprimirzip.png">
     </div>
     
     <div class="col-10 col-md-6 col-lg-5">
     <a href='https://wp-kama.com/function/PclZip::extract' class='cascadia-code'>PclZip::extract($ruta)</a><br>
-    <img class="img-fluid" src="../Imagenes/descomprimirpclzip.png">
+    <img class="img-fluid mt-2" src="../Imagenes/descomprimirpclzip.png">
     </div>
   
     </div>
@@ -78,10 +78,10 @@
       </div>
     </div>
       <div class="row align-items-center justify-content-center">
-        <div class="">
+        <div class="col-10">
           <p>1. Descargar la siguiente librería de pclzip: <a class="alert-link" download href='../../Util/pclzip.lib.php'>Libreria PclZip</a></p>      
           <p>2. Poner la libreria en la página en donde esten las funciones utiles</p>
-          <p>3. Incluir el archivo en los archivos de control de comprimir y descomprimir con<b>include_once('[ruta]/pclzip.lib.php')</b></p> 
+          <p>3. Incluir el archivo en los archivos de control de comprimir y descomprimir con <span class='cascadia-code'>include_once('[ruta]/pclzip.lib.php')</span></p> 
           <p>4. Luego para realizar la función de la librería guiarse de los pasos que nosotras realizamos(me falta un ancla)</p>  
     </div>
   </div>
